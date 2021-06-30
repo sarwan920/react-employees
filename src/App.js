f79677dbff6df6def4ed3a8778a15a28ce1e90c8
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2021-06-30 23:04:34 +0500

    after the code update

--- src/App.js
+++ src/App.js
@@ -14,43 +14,70 @@ import { useToast } from "@chakra-ui/react";
 
 import { Spinner, Center } from "@chakra-ui/react";
 
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_EMPLOYEES, ADD_EMPLOYEE, DELETE_EMPLOYEE } from "./client/client";
 
 //MAIN APP FUNCTION COMPONENT
 function App() {
-  return (
-    <Container maxW="container.xl">
-     
-      <VStack
-        boxShadow="md"
-        mt={50}
-        p={10}
-        border="ridge"
-        spacing={10}
-        align="stretch"
-      >
-        <Box>
-          <AddEmployee></AddEmployee>
-        </Box>
-        <Box>
-          <EmployeeTable></EmployeeTable>
-        </Box>
-      </VStack>
-    </Container>
-  );
-}
-
-//SHOW EMPLOYEE COMPONENT FUNCTION
-function EmployeeTable() {
-  const { data, loading } = useQuery(GET_EMPLOYEES);
+  const [addEmployee, { loading: addEmployees }] = useMutation(ADD_EMPLOYEE);
+  const { data, loading: getEmployees } = useQuery(GET_EMPLOYEES);
   const [delete_employee] = useMutation(DELETE_EMPLOYEE);
   const toast = useToast();
 
-  const editEmployee = ({ id }) => {
-    alert(id);
+  const [edit, setEdit] = useState(false);
+
+
+
+  const addNewEmployee = () => {
+    setEdit(false);
+    console.log(edit);
+    document.getElementById("employee_form").reset();
+  }
+
+
+  //ADD EMPLOYEE LOGIC
+  const nameRef = useRef();
+  const surnameRef = useRef();
+  const addressRef = useRef();
+  const postcodeRef = useRef();
+  const telephoneRef = useRef();
+  const emailRef = useRef();
+
+  const updateEmployee=()=>{
+    console.log('Update Employee');
+  }
+
+  const saveEmployee = (event) => {
+    const enteredName = nameRef.current.value;
+    const enteredSurname = surnameRef.current.value;
+    const enteredAddress = addressRef.current.value;
+    const enteredPostcode = postcodeRef.current.value;
+    const enteredTelephone = telephoneRef.current.value;
+    const enteredEmail = emailRef.current.value;
+
+    addEmployee({
+      variables: {
+        name: enteredName,
+        surname: enteredSurname,
+        address: enteredAddress,
+        postcode: enteredPostcode,
+        telephone: enteredTelephone,
+        email: enteredEmail,
+      },
+      refetchQueries: [{ query: GET_EMPLOYEES }],
+    }).then(() => {
+      document.getElementById("employee_form").reset();
+      toast({
+        title: "Employee Saved Successfully!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top",
+        variant: "top-accent",
+      });
+    });
   };
 
   const deleteEmployee = ({ id }) => {
@@ -84,141 +111,150 @@ function EmployeeTable() {
     }
   };
 
-  if (loading) {
+  const editEmployee = (employee) => {
+    setEdit(true);
+    nameRef.current.value = employee.name;
+    surnameRef.current.value = employee.surname;
+    addressRef.current.value = employee.address;
+    postcodeRef.current.value = employee.postcode;
+    telephoneRef.current.value = employee.telephone;
+    emailRef.current.value = employee.email;
+
+    console.log(edit);
+
+
+  }
+
+  //Spinner if its getting employees or employees are loading
+  if (getEmployees) {
     return (
-      <Center>
+      <Center mt={80}>
         {" "}
-        <Spinner size="lg" />
+        <Spinner size="xl" />
       </Center>
     );
   }
 
   return (
-    <Table>
-      {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
-      <Thead bg="black">
-        <Tr>
-          <Th color="white">Name</Th>
-          <Th color="white">Surname</Th>
-          <Th color="white">Address</Th>
+    <Container maxW="container.xl">
+      <VStack
+        boxShadow="md"
+        mt={50}
+        p={10}
+        border="ridge"
+        spacing={10}
+        align="stretch"
+      >
+        <Button
+          onClick={addNewEmployee}
+        // onClick={ toggleEdit }
+        >Add New Employee</Button>
+        {/* ADD EMPLOYEE FORM COMPONENT */}
+        <Box>
+          <form id="employee_form">
+            <Flex>
+              <Center>
+                <Stack spacing={5} m="5">
+                  <Input type="text" placeholder="Name" ref={nameRef} />
 
-          <Th color="white">Postcode</Th>
-          <Th color="white">Telephone</Th>
-          <Th color="white">Email Address</Th>
-          <Th></Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.employees.map((employee) => (
-          <Tr
-            _hover={{ bg: "#E2E8F0", cursor: "pointer" }}
-            id={employee.id}
-            key={employee.id}
-          >
-            <Td>{employee.name}</Td>
-            <Td>{employee.surname}</Td>
-            <Td>{employee.address}</Td>
-            <Td>{employee.postcode}</Td>
-            <Td>{employee.telephone}</Td>
-            <Td>{employee.email}</Td>
-            <Td>
-              <Button
-                
-                // onClick={() => editEmployee(employee)}
-                _hover={{ bg: "green" }}
-                mr="2"
-                bg="black"
-                color="white"
-              >
-                Edit
-              </Button>
-              <Button
-                onClick={() => deleteEmployee(employee)}
-                _hover={{ bg: "green" }}
-                bg="black"
-                color="white"
-              >
-                Delete
-              </Button>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
-  );
-}
+                  <Input type="text" placeholder="Surname" ref={surnameRef} />
+                </Stack>
 
-function AddEmployee(props) {
-  const [addEmployee, { loading }] = useMutation(ADD_EMPLOYEE);
-  const toast = useToast();
-  const nameRef = useRef();
-  const surnameRef = useRef();
-  const addressRef = useRef();
-  const postcodeRef = useRef();
-  const telephoneRef = useRef();
-  const emailRef = useRef();
+                <Stack spacing={5}>
+                  <Input type="text" placeholder="Address" ref={addressRef} />
 
-  const saveEmployee = (event) => {
-    const enteredName = nameRef.current.value;
-    const enteredSurname = surnameRef.current.value;
-    const enteredAddress = addressRef.current.value;
-    const enteredPostcode = postcodeRef.current.value;
-    const enteredTelephone = telephoneRef.current.value;
-    const enteredEmail = emailRef.current.value;
+                  <Input
+                    type="text"
+                    placeholder="Post Code"
+                    ref={postcodeRef}
+                  />
+                </Stack>
 
-    addEmployee({
-      variables: {
-        name: enteredName,
-        surname: enteredSurname,
-        address: enteredAddress,
-        postcode: enteredPostcode,
-        telephone: enteredTelephone,
-        email: enteredEmail,
-      },
-      refetchQueries: [{ query: GET_EMPLOYEES }],
-    }).then(() => {
-      document.getElementById("employee_form").reset();
-      toast({
-        title: "Employee Saved Successfully!",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-        position: "top",
-        variant: "top-accent",
-      });
-    });
-  };
+                <Stack spacing={5} m="5">
+                  <Input
+                    type="text"
+                    placeholder="Telephone"
+                    ref={telephoneRef}
+                  />
 
-  return (
-    <form id="employee_form">
-      <Flex>
-        <Center>
-          <Stack spacing={5} m="5">
-            <Input type="text" placeholder="Name" ref={nameRef} />
+                  <Input
+                    type="text"
+                    placeholder="Email Address"
+                    ref={emailRef}
+                  />
+                </Stack>
 
-            <Input type="text" placeholder="Surname" ref={surnameRef} />
-          </Stack>
+                <Stack spacing={5}>
+                  <Button isLoading={addEmployees} onClick={ !edit ?  saveEmployee :updateEmployee}>
+                    Save
+                  </Button>
+                  { edit && <Button>Cancel</Button> }
+                    
 
-          <Stack spacing={5}>
-            <Input type="text" placeholder="Address" ref={addressRef} />
+                </Stack>
+              </Center>
+            </Flex>
+          </form>
+        </Box>
 
-            <Input type="text" placeholder="Post Code" ref={postcodeRef} />
-          </Stack>
+        {/* Show Employee Table Component */}
+        <Box>
+          <Table>
+            <Thead bg="black">
+              <Tr>
+                <Th color="white">Name</Th>
+                <Th color="white">Surname</Th>
+                <Th color="white">Address</Th>
 
-          <Stack spacing={5} m="5">
-            <Input type="text" placeholder="Telephone" ref={telephoneRef} />
+                <Th color="white">Postcode</Th>
+                <Th color="white">Telephone</Th>
+                <Th color="white">Email Address</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.employees.map((employee) => (
+                <Tr
+                  _hover={{ bg: "#E2E8F0", cursor: "pointer" }}
+                  id={employee.id}
+                  key={employee.id}
+                >
+                  <Td>{employee.name}</Td>
+                  <Td>{employee.surname}</Td>
+                  <Td>{employee.address}</Td>
+                  <Td>{employee.postcode}</Td>
+                  <Td>{employee.telephone}</Td>
+                  <Td>{employee.email}</Td>
+                  <Td>
+                    <Button
+                      // onClick={toggleEdit}
+                      onClick={() => editEmployee(employee)}
+                      _hover={{ bg: "green" }}
+                      mr="2"
+                      bg="black"
+                      color="white"
+                    >
+                      Edit
+                    </Button>
+                    <Button
 
-            <Input type="text" placeholder="Email Address" ref={emailRef} />
-          </Stack>
 
-          <Stack spacing={5}>
-            <Button isLoading={loading} onClick={saveEmployee}>
-              Save
-            </Button>
-          </Stack>
-        </Center>
-      </Flex>
-    </form>
+
+                      onClick={() => deleteEmployee(employee)}
+                      _hover={{ bg: "green" }}
+                      bg="black"
+                      color="white"
+                    >
+                      Delete
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </VStack>
+    </Container>
   );
 }
 
